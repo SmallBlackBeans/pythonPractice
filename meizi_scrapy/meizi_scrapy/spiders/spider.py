ec5d4dd50a81f7d4dad2d34e2b89927d1322a3e5
韩承海 <374802597@qq.com>
2018-07-12 18:45:23 +0800

    no message

--- meizi_scrapy/meizi_scrapy/spiders/spider.py
+++ meizi_scrapy/meizi_scrapy/spiders/spider.py
@@ -1,4 +1,4 @@
-from scrapy import Request
+from scrapy import Request,http
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 from meizi_scrapy.items import MeiziScrapyItem
@@ -14,9 +14,13 @@ class Spider(CrawlSpider):
              callback='parse_item', follow=True),
     )
 
-    def parse_item(self, response):
+    def parse_item(self, response:http.Response):
         item = MeiziScrapyItem()
         max_num = response.xpath(
             "descendant::div[@class='main']/div[@class='content']/div[@class='pagenavi']/a[last()-1]/span/text()").extract_first(
             default="N/A")
-        item['name'] = response.xpath()
+        item['name'] = response.xpath("./*//div[@class='main']/div[1]/h2/text()").extract_first(default="N/A")
+        item['url'] = response.url
+        for num in range(1,int(max_num)):
+            # page_url =
+    
